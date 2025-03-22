e6f99a10fdacc2b066d127cdef153b8ae7215a59
adesola <ava6969@rit.edu>
2025-03-21 18:29:22 -0700

    fixed vesion

--- cmake/EpochCoreCPM.cmake
+++ cmake/EpochCoreCPM.cmake
@@ -4,10 +4,23 @@
 # Usage: include(EpochCoreCPM.cmake)
 
 set(EPOCH_CORE_REPOSITORY "https://github.com/EPOCHDevs/EpochCore.git" CACHE STRING "EpochCore repository URL")
-set(EPOCH_CORE_TAG "main" CACHE STRING "EpochCore Git tag to use")
+set(EPOCH_CORE_TAG "master" CACHE STRING "EpochCore Git tag to use")
 
 if(NOT TARGET epoch::core)
-    include(CPM)
+    # Download CPM.cmake if not already available
+    set(CPM_DOWNLOAD_VERSION 0.38.7)
+    set(CPM_DOWNLOAD_LOCATION "${CMAKE_BINARY_DIR}/cmake/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
+    
+    if(NOT EXISTS ${CPM_DOWNLOAD_LOCATION})
+        message(STATUS "Downloading CPM.cmake to ${CPM_DOWNLOAD_LOCATION}")
+        file(DOWNLOAD
+            https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
+            ${CPM_DOWNLOAD_LOCATION}
+        )
+    endif()
+    
+    include(${CPM_DOWNLOAD_LOCATION})
+    
     CPMAddPackage(
         NAME EpochCore
         GIT_REPOSITORY ${EPOCH_CORE_REPOSITORY}
